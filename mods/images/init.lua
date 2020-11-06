ccc2eb877ebc2041536ab25203692e8b53cff726
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-11-06 11:53:58 +0100

    add decoder demo and images

--- mods/images/init.lua
+++ mods/images/init.lua
@@ -1,19 +1,36 @@
 
-minetest.register_node("images:circuit_demo", {
-	description = "Circuit demo",
-	drawtype = "signlike",
-	tiles = {"images_circuit_demo_schematics.jpg"},
-	visual_scale = 3.0,
-	inventory_image = "images_circuit_demo_schematics.jpg",
-	wield_image = "images_circuit_demo_schematics.jpg",
-	paramtype = "light",
-	paramtype2 = "wallmounted",
-	sunlight_propagates = true,
-	walkable = false,
-	selection_box = {
-		type = "wallmounted",
+local images = {
+	{
+		texture = "images_circuit_demo_schematics.jpg",
+		name = "circuit_demo",
+		description = "Circuit demo"
 	},
-	groups = {
-		choppy = 1
+	{
+		texture = "images_decoder_demo_schematics.jpg",
+		name = "decoder_demo",
+		description = "Decoder demo"
 	}
-})
+}
+
+for _, def in ipairs(images) do
+	minetest.register_node("images:" .. def.name, {
+		description = def.description,
+		drawtype = "signlike",
+		tiles = {
+			def.texture
+		},
+		visual_scale = 3.0,
+		inventory_image = def.texture,
+		wield_image = def.texture,
+		paramtype = "light",
+		paramtype2 = "wallmounted",
+		sunlight_propagates = true,
+		walkable = false,
+		selection_box = {
+			type = "wallmounted",
+		},
+		groups = {
+			choppy = 1
+		}
+	})
+end
