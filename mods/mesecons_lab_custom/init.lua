4cb971ac559dc3b1f9d902029122e45eae666943
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-06-10 15:44:37 +0200

    merge mods

--- mods/mesecons_lab_custom/init.lua
+++ mods/mesecons_lab_custom/init.lua
@@ -8,3 +8,21 @@ minetest.override_item("doors:door_steel_c", {
     mesecons = door_def.mesecons
 })
 
+minetest.register_on_joinplayer(function(player)
+    player:set_clouds({ density=0 })
+    player:set_sky({r=0, g=0, b=0}, "skybox", {
+        "heaven_up.jpg^[transformR270",
+        "heaven_dn.jpg^[transformR90",
+        "heaven_ft.jpg",
+        "heaven_bk.jpg",
+        "heaven_lf.jpg",
+        "heaven_rt.jpg"
+    })
+end)
+
+local function move_to_spawn(player)
+    player:set_pos({ x=0, y=2, z=0 })
+end
+
+minetest.register_on_respawnplayer(move_to_spawn)
+minetest.register_on_newplayer(move_to_spawn)
--- mods/spawn/init.lua
+++
@@ -1,6 +0,0 @@
-local function move_to_spawn(player)
-    player:set_pos({ x=0, y=2, z=0 })
-end
-
-minetest.register_on_respawnplayer(move_to_spawn)
-minetest.register_on_newplayer(move_to_spawn)
