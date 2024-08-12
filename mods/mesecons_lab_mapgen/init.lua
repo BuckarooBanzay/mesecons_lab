6db98ae36eca762ae8dd81e8b37f415d3255248c
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-08-12 17:04:22 +0200

    add data backend

--- mods/mesecons_lab_mapgen/init.lua
+++ mods/mesecons_lab_mapgen/init.lua
@@ -1,4 +1,11 @@
+local MP = minetest.get_modpath("mesecons_lab_mapgen")
+
 mapsync.register_backend("default-backend", {
     type = "fs",
-    path = minetest.get_modpath("mesecons_lab_mapgen") .. "/map"
+    path = MP .. "/map"
+})
+
+mapsync.register_data_backend({
+    type = "fs",
+    path = MP .. "/data"
 })
